369ae5e06f50bd533bf6bbf5dfef9f8d51514052
Andrew Rawson <arawson@proton.me>
2024-12-12 23:02:17 -0500

    integrated prints into main script and added a loop

--- source/script.py
+++ source/script.py
@@ -1,9 +1,14 @@
 # main script to handle interactions 
 from data import load_data
 from graph import Graph, Vertex # at this stage I don't know if script.py needs graph interaction itself
+from prints import welcome, greeting, line_break, double_line_break, goodbye_message
 
+print(welcome())
+print(line_break())
+print(greeting())
+print(double_line_break())
 def get_user_preferences():
-    genre = input("Enter a genre to get recommendations: ").strip().title()
+    genre = input("  Enter a genre to get recommendations: ").strip().title()
     return genre
 
 def recommend_games(graph, genre):
@@ -13,12 +18,19 @@ def recommend_games(graph, genre):
     return sorted_games
 
 def main():
-    graph = load_data()
-    genre = get_user_preferences()
-    recommendations = recommend_games(graph, genre)
-    print(f"\nTop games in {genre}:\n")
-    for game in recommendations:
-        print(f"\n{game.value} with rating {game.rating}\n")
+    while True:
+        graph = load_data()
+        genre = get_user_preferences()
+        recommendations = recommend_games(graph, genre)
+        print(line_break())
+        print(f"  Top games in {genre}:\n")
+        for game in recommendations:
+            print(f"\n  {game.value} with rating {game.rating}\n")
+        print(line_break())
+        choose = input("  Would you like to look for more recommendations? <yes/no>: ").strip().lower()
+        if choose != "yes":
+            print(goodbye_message())
+            break
 
 if __name__ == "__main__":
     main()
